fd09273f337e31dc72f0b0e9666c2d708d76390a
Carla Fernández González <carlafgpitando@gmail.com>
2016-05-07 14:09:46 +0200

    Updated song-renaming, now working with subfolders

--- SongRenaming/SongRenaming/Program.cs
+++ SongRenaming/SongRenaming/Program.cs
@@ -5,52 +5,52 @@ using System.Text;
 
 namespace SongRenaming {
     class Program {
+
         /// <summary>
         /// Converts all song files from the given path to the standard:
         /// numTrack - songTitle
         /// </summary>
-        /// <param name="args">File path</param>
+        /// <param name="args">File path, subfolder renaming</param>
         static void Main(string[] args) {
             string[] extensions = new string[] { ".mp3",".flac", ".rec",
             ".m4a",".aac",".wav",".mp4a", ".wma", ".ogg",};
-            var path = @args[0];
+            string path;
+            if (args.Count() >= 1)
+                path = @args[0];
+            else {
+                Console.WriteLine("You must write the path of your files");
+                return;
+            }
             bool renameSubfolders;
-            bool areThereSubfolders;
-            // TODO: exceptions
-            Boolean.TryParse(args[1], out renameSubfolders);
-            areThereSubfolders = Directory.GetDirectories(path).Count().Equals(0);
-            Console.WriteLine(areThereSubfolders);
-            // TODO: make it work with subfolders
-            // if there are subfolders and subfolder renaming is checked, rename everything
-            //var filepaths = areThereSubfolders && renameSubfolders ?
-            //    Directory.GetDirectories(path).ToArray().SelectMany(dir => Directory.GetFiles(dir)).Where(file => {
-            //        foreach (string extension in extensions) {
-            //            if (Path.GetExtension(file).Equals(extension))
-            //                return true;
-            //        }
-            //        return false;
-            //    })
-            //// otherwise, rename only the files in the path folder
-            //:
-            //Directory.GetFiles(path).Where(file => {
-            //    foreach (string extension in extensions) {
-            //        if (file.ToString().Contains(extension))
-            //            return true;
-            //    }
-            //    return false;
-            //});
+            if (args.Count() == 2)
+                Boolean.TryParse(args[1], out renameSubfolders);
+            else renameSubfolders = false;
 
-            //foreach (var filepath in filepaths) {
-            //    TagLib.File file = TagLib.File.Create(@filepath);
-            //    StringBuilder sb = new StringBuilder();
-            //    sb.Append(file.Tag.Track).Append(" - ");
-            //    sb.Append(file.Tag.Title);
-            //    var pathWithoutFileName = Path.GetDirectoryName(filepath);
-            //    var newFilePath = Path.Combine(pathWithoutFileName, sb.ToString() + Path.GetExtension(filepath));
-            //    Console.WriteLine("Renaming file {0} to {1}", filepath, newFilePath);
-            //    File.Move(filepath, newFilePath);
+            var filesInCurrentDir = Directory.GetFiles(path)
+                .Where(file => extensions.Contains(Path.GetExtension(file))).ToList();
+            var filesInSubDirs = Directory.GetDirectories(path)
+                .SelectMany(dir => Directory.GetFiles(dir)
+                .Where(file => extensions.Contains(Path.GetExtension(file)))).ToList();
 
-            //}
+            filesInCurrentDir.ForEach(filepath => Rename(filepath));
+            if (renameSubfolders)
+                filesInSubDirs.ForEach(filepath => Rename(filepath));
+
+        }
+
+        /// <summary>
+        /// Applies the renaming algorithm to the current file
+        /// </summary>
+        /// <param name="filepath"></param>
+        private static void Rename(string filepath) {
+            TagLib.File file = TagLib.File.Create(@filepath);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(file.Tag.Track).Append(" - ");
+            sb.Append(file.Tag.Title);
+            var pathWithoutFileName = Path.GetDirectoryName(filepath);
+            var newFilePath = Path.Combine(pathWithoutFileName, sb.Append(Path.GetExtension(filepath)).ToString());
+            Console.WriteLine("Renaming file {0} to {1}", Path.GetFileName(filepath), Path.GetFileName(newFilePath));
+            File.Move(filepath, newFilePath);
 
         }
     }
